c27b351d2a10a3e26ae14cdcab4208b21b961963
DrakeDussault <drake.dussault@thegso.com>
2024-05-31 12:55:23 -0400

    Build color mode switch

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,11 +1,15 @@
 import { HStack, Image, Text } from "@chakra-ui/react";
 import logo from "../assets/logo/logo.webp";
+import ColorModeSwitch from "./ColorModeSwitch";
 
 function navbar() {
   return (
-    <HStack>
-      <Image src={logo} boxSize="60px"></Image>
-      <Text>Game Hub</Text>
+    <HStack justifyContent="space-between">
+      <HStack>
+        <Image src={logo} boxSize="60px"></Image>
+        <Text>Game Hub</Text>
+      </HStack>
+      <ColorModeSwitch />
     </HStack>
   );
 }
