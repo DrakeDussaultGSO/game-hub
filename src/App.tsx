460a0ed1622a1d3aaeaf4fbab42ed85f8381b816
DrakeDussault <drake.dussault@thegso.com>
2024-06-01 10:52:06 -0400

    General Cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Grid, GridItem, HStack, Show, Text } from "@chakra-ui/react";
+import { Box, Grid, GridItem, HStack, Show, Text } from "@chakra-ui/react";
 import NavBar from "./components/NavBar";
 import GameGrid from "./components/GameGrid";
 import GenreList from "./components/GenreList";
@@ -7,6 +7,7 @@ import { Genre } from "./hooks/useGenres";
 import PlatformSelector from "./components/PlatformSelector";
 import { Platform } from "./hooks/useGames";
 import SortSelector from "./components/SortSelector";
+import Header from "./components/Header";
 
 export interface GameQuery {
   genre: Genre | null;
@@ -47,21 +48,24 @@ function App() {
         </GridItem>
       </Show>
       <GridItem area="main">
-        <HStack paddingLeft={2} marginBottom={5}>
-          <PlatformSelector
-            selectedPlatform={gameQuery.platform}
-            platformSelected={(platform) =>
-              setGameQuery({ ...gameQuery, platform: platform })
-            }
-          />
-          <SortSelector
-            sortOrder={gameQuery.sortOrder}
-            onSelectSortOrder={(sortOrder) =>
-              setGameQuery({ ...gameQuery, sortOrder: sortOrder })
-            }
-          />
-        </HStack>
+        <Box paddingLeft={2}>
+          <Header gameQuery={gameQuery} />
+          <HStack marginBottom={5}>
+            <PlatformSelector
+              selectedPlatform={gameQuery.platform}
+              platformSelected={(platform) =>
+                setGameQuery({ ...gameQuery, platform: platform })
+              }
+            />
 
+            <SortSelector
+              sortOrder={gameQuery.sortOrder}
+              onSelectSortOrder={(sortOrder) =>
+                setGameQuery({ ...gameQuery, sortOrder: sortOrder })
+              }
+            />
+          </HStack>
+        </Box>
         <GameGrid gameQuery={gameQuery} />
       </GridItem>
     </Grid>
---
+++ src/components/Header.tsx
@@ -0,0 +1,19 @@
+import { Heading } from "@chakra-ui/react";
+import { GameQuery } from "../App";
+
+interface Props {
+  gameQuery: GameQuery;
+}
+
+function Header({ gameQuery }: Props) {
+  return (
+    <>
+      <Heading as="h1" marginY={5} fontSize="5xl">
+        {gameQuery.platform && gameQuery.platform.name + " "}
+        {gameQuery.genre && gameQuery.genre?.name + " "}Games
+      </Heading>
+    </>
+  );
+}
+
+export default Header;
